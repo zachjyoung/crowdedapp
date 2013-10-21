1ec4c97892fec3053939a15684863be78c46a39d
Zach Young <zachjyoung@gmail.com>
2013-10-21 12:23:14 -0400

    initial commit removed database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crowdedapp::Application.config.secret_key_base = '46f9fefad881c92f6d449c02f8c6993f5b19007857cd190f61688380276bf1c276bd6fcfe08264f49132fa4f385c99a5cac921c0824ee74eebe0d579fea25b49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Crowdedapp::Application.config.session_store :cookie_store, key: '_crowdedapp_session'
