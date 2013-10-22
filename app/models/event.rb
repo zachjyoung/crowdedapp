c099890c85c24ac59b9765eb5c229181765bc87d
Zach Young <zachjyoung@gmail.com>
2013-10-22 03:27:03 -0400

    going to heroku

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
 
   validates_numericality_of :severity
   validates_presence_of :severity
+  validates_presence_of :location
+  validates_presence_of :title
 
   def self.severity
     (1..5).to_a
