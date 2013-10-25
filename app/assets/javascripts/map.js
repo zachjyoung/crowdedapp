7425e05599369cfbfaff538ebd56bbf58c28191c
Zach Young <zachjyoung@gmail.com>
2013-10-25 19:09:30 -0400

    added more map data

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
 
-var map = L.mapbox.map('map','zachyoung.map-xp4e2t1j').setView([51.505, -0.09], 13);
+$.getJSON( "http://smart-ip.net/geoip-json?callback=?", function(data){
+var map = L.mapbox.map('map','zachyoung.map-xp4e2t1j').setView([data.latitude, data.longitude], 13);
 
 // L.tileLayer('http://{s}.tile.cloudmade.com/API-key/997/256/{z}/{x}/{y}.png', {
 //     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
 //     maxZoom: 18
 // }).addTo(map);
 
-var circle = L.circle([51.508, -0.11], 500, {
+var circle = L.circle([data.latitude, data.longitude], 500, {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5
 }).addTo(map);
+
+
+});
 
 });
